510bf37c68a57df4bfd78001ecaf66a6ccf26b97
dryjorse <116591223+dryjorse@users.noreply.github.com>
2024-11-12 16:57:47 +0600

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -19,4 +19,4 @@ export const qrMessageAtom = atom("");
 export const isAuthAtom = atom(false);
 export const paymentStatusAtom = atom<PaymentStatusType>("success");
 
-export const isClosedAtom = atom(true);
+export const isClosedAtom = atom(false);
